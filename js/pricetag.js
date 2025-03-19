749155c89d23fa92407b5118a5b1d1ebcc7560cb
Ida-Sofie Karlsson <ida.sofie.karlsson@gmail.com>
2025-03-19 14:00:10 +0100

    assignment done

---
+++ js/pricetag.js
@@ -0,0 +1,6 @@
+let input = prompt("Enter the prize. You must start with the symbol for your local currency and use . instead of ,: ");
+let prizeAsText = input.slice(1);
+let currencySymbol = input.slice(0,1);
+let prizeAsNumber = parseFloat(prizeAsText);
+let prizeAfterDiscount = 0.9 * prizeAsNumber;
+console.log(`You have received a 10% discount! Your new prize is ${currencySymbol}${prizeAfterDiscount}`);
